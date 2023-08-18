581a277e92e3dd00bed45d9171a16a462d1d3e1d
AidenRoh <colohrby@gmail.com>
2023-08-18 13:22:43 +0900

    230818

--- item-service/src/main/java/hello/itemservice/domain/item/Item.java
+++ item-service/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,6 +1,5 @@
 package hello.itemservice.domain.item;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -77,11 +78,25 @@ public class BasicItemController {
 //
 //        return "basic/item ";
 //    }
+//
+//    @PostMapping("/add")
+//    public String addItemV4(Item item) {
+//        itemRepository.save(item);
+//        return "basic/item ";
+//    }
+
+//    @PostMapping("/add")
+//    public String addItemV5(Item item) {
+//        itemRepository.save(item);
+//        return "redirect:/basic/items/" + item.getId();
+//    }
 
     @PostMapping("/add")
-    public String addItemV4(Item item) {
-        itemRepository.save(item);
-        return "basic/item ";
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
@@ -96,4 +111,6 @@ public class BasicItemController {
         itemRepository.update(itemId, item);
         return "redirect:/basic/items/{itemId}";
     }
+
+
 }
